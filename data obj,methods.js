1275f1deb70bc0b7190d1d23fe632df4f8a8ef74
BalaSivaPrasad <balasivaprasad4@gmail.com>
2025-03-29 16:27:17 +0530

    Create data obj,methods.js
    data object properties and methods

---
+++ data obj,methods.js
@@ -0,0 +1,7 @@
+<script>
+let today = new Date();
+console.log("Current Date:", today);
+console.log("Year:", today.getFullYear());
+console.log("Month:", today.getMonth() + 1);
+console.log("Day:", today.getDate());
+</script>
