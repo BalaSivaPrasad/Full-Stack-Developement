70051cb038ae50768c4c3feb1a579193b353818e
BalaSivaPrasad <balasivaprasad4@gmail.com>
2025-03-29 16:22:05 +0530

    Create arr objects,methods.js
    Array object properties and methods

---
+++ arr objects,methods.js
@@ -0,0 +1,9 @@
+<script>
+let numbers = [10, 20, 30, 40, 50];
+console.log("Original Array:", numbers);
+numbers.push(60);
+console.log("After Push:", numbers);
+numbers.pop();
+console.log("After Pop:", numbers);
+console.log("Array Length:", numbers.length);
+</script>
