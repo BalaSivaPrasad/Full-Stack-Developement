cfaec31704b09125c2f59f8b6ca93bff91d085a7
BalaSivaPrasad <balasivaprasad4@gmail.com>
2025-03-29 16:23:10 +0530

    Create math objects,methods.js
    math object properties and methods

---
+++ math objects,methods.js
@@ -0,0 +1,6 @@
+<script>
+console.log("Math.PI:", Math.PI);
+console.log("Random Number:", Math.random());
+console.log("Square Root of 16:", Math.sqrt(16));
+console.log("Power (2^5):", Math.pow(2, 5));
+</script>
